8e0ee899036c95a6c5bc2f88fb7c5b120137c7be
Divyansh Yadav <indrapalsingh12july2005@gmail.com>
2025-08-27 13:00:42 +0530

    27 August 2025

---
+++ src/MyPrint.java
@@ -0,0 +1,8 @@
+public class MyPrint implements Printable {
+    public void print() {
+        System.out.println("Print Method id implemented");
+    }
+    public void show(){
+        System.out.println("show method is implemented");
+    }
+}
